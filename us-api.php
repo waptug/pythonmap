996529794aeec069c409e2ad13c1d92228a7e3d9
Michael McGinn <waptug@gmail.com>
2023-09-17 01:39:59 -0700

    modified us-api.php file to work with new us.json format

--- us-api.php
+++ us-api.php
@@ -1,18 +1,22 @@
 <?php
 
 // Read JSON file
-$json_file_path = 'us.json';
+$json_file_path = 'path/to/us.json';
 $json_data = file_get_contents($json_file_path);
 $data = json_decode($json_data, true);
 
 // Define REST endpoint
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['state'])) {
-    $state = $_GET['state'];
-    $filtered_data = array_filter($data, function($row) use ($state) {
-        return $row['Alaska'] == $state;
-    });
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $filtered_data = $data;
+    foreach ($_GET as $key => $value) {
+        if (array_key_exists($key, $data[0])) {
+            $filtered_data = array_filter($filtered_data, function($row) use ($key, $value) {
+                return $row[$key] == $value;
+            });
+        }
+    }
     header('Content-Type: application/json');
-    echo json_encode($filtered_data);
+    echo json_encode(array_values($filtered_data));
 } else {
     header('HTTP/1.1 405 Method Not Allowed');
     header('Allow: GET');
